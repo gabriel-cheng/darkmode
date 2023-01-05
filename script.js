43797653476c01e6d770f9cf90906981858caf10
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-04 22:35:56 -0300

    feat(script): adicionando posição correta do botão e finalizando o projeto

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const btnCheck = document.querySelector("#checkbox");
 
 btnCheck.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+    const body = document.body;
+    const ball = document.querySelector(".ball");
+    localStorage.setItem("theme", null);
+
+    body.classList.toggle("dark");
+
+    ball.style.transition = "transform .2s linear";
+
+    if(body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    }
 });
+
+window.onload = () => {
+    const temaAtual = localStorage.getItem("theme");
+    const ball = document.querySelector(".ball");
+
+    if(temaAtual === "dark") {
+        const body = document.body;
+        body.classList.add("dark");
+        
+        document.querySelector("#checkbox").checked = true;
+
+
+        return;
+    }
+}
